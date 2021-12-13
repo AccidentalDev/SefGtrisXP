8f932097ffdf8321d067794fa3c83e2a6982dcb0
AccidentalDev <serge_cfcu@hotmail.com>
2021-12-12 18:56:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/sefgtrisxp/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.sefgtrisxp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    fun onClickedExit() {}
+}
